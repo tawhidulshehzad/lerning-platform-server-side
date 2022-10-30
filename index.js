a335b9b4b9289f38d1b0dd8a7fa93a7a51b62b39
shehzad1212 <tawhidulislam261@gmail.com>
2022-10-30 17:37:08 +0600

    text added

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 
 const categories = require("./data/categories.json");
+// Classes data take
+const classes = require("./data/classes.json");
 
 // Basic api call
 app.get("/", (req, res) => {
@@ -15,6 +17,30 @@ app.get("/", (req, res) => {
 // Category API call
 app.get("/categories", (req, res) => {
   res.send(categories);
+});
+
+// classes acording to categories
+app.get("/category/:id", (req, res) => {
+  const id = req.params.id;
+  if (id === "08") {
+    res.send(classes);
+  } else {
+    const categoryClass = classes.filter((c) => c.category_id === id);
+    res.send(categoryClass);
+  }
+});
+
+// all class loading
+
+app.get("/classes", (req, res) => {
+  res.send(classes);
+});
+
+// classes api call
+app.get("/classes/:id", (req, res) => {
+  const id = req.params.id;
+  const slectedClass = classes.find((c) => c._id === id);
+  res.send(slectedClass);
 });
 
 app.listen(port, () => {
